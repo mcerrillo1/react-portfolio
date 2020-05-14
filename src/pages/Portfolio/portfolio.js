c47c9c87b6bf2b3771796db909c8221fd0319af5
mcerrillo1 <mcerrillo1@yahoo.com>
2020-05-13 23:21:06 -0700

    initial

---
+++ src/pages/Portfolio/portfolio.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function PortfolioInfo() {
+  
+  return (
+    <div className="card">
+      <div>
+        Melvin Cerrillo
+      </div>
+      <div>
+        About:
+      </div>
+      <div>
+        Excitement Level:
+        </div>
+    </div>
+  );
+}
+
+export default PortfolioInfo;
